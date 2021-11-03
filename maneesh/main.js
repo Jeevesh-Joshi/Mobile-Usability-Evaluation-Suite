dcd11d5fbec31c1c3252ee4b1d61e9f5026a1c2e
ManeeshGuptaNvss <48683390+ManeeshGuptaNvss@users.noreply.github.com>
2021-11-03 18:03:49 +0530

    working code

--- maneesh/main.js
+++ maneesh/main.js
@@ -4,19 +4,21 @@ let users = [
     name: "Maneesh",
     age: 21,
     gender: " Male",
-    tasks:['1. login']
+    tasks: ["1. login"],
   },
   {
     id: 2,
     name: "Gupta",
     age: 22,
     gender: " Male",
+    tasks: ["1. login"],
   },
   {
     id: 3,
     name: "alexa",
     age: 29,
     gender: " Female",
+    tasks: ["1. login"],
   },
 ];
 
@@ -24,17 +26,19 @@ let printData = () => {
   document.getElementById("data").innerHTML = "";
   let i = 0;
   users.map((x) => {
+    
     document.getElementById("data").innerHTML += `
         <tr>
-        <td >${x.id}</td>
+        <td >${i + 1}</td>
         <td >${x.name}</td>
         <td >${x.age}</td>
         <td >${x.gender}</td>
+        <td >${x.tasks}</td>
         </tr>
         `;
     i += 1;
   });
-  console.log("printdata");
+  // console.log("printdata");
 };
 printData();
 
@@ -43,24 +47,28 @@ let addData = (e) => {
   console.log("Add data");
   let name = document.getElementById("name").value;
   let age = parseInt(document.getElementById("age").value);
-  let radiobts=document.querySelectorAll('input[name="gender"]')
-  let checkboxes=document.querySelectorAll('input[name="tasks"]')
-  let gender
-  for(let rbt of radiobts  )
-  {
-      if(rbt.checked){
-          gender=rbt.value;
-        //   console.log(gender)
-          break;
-      }
+  let radiobts = document.querySelectorAll('input[name="gender"]');
+  let checkboxes = document.querySelectorAll('input[name="tasks"]:checked');
+  let tasks = [];
+  let gender;
+  for (let rbt of radiobts) {
+    if (rbt.checked) {
+      gender = rbt.value;
+      //   console.log(gender)
+      break;
+    }
   }
+  checkboxes.forEach((checkbox) => {
+    tasks.push(checkbox.value);
+  });
+  console.log(tasks)
   let dataToAdd = {
-    id: Date.now(),
     name,
     age,
     gender,
+    tasks,
   };
-  if (name.length && age ) {
+  if (name.length && age && tasks.length > 0) {
     users.unshift(dataToAdd);
   } else {
     alert("Something is missing");
