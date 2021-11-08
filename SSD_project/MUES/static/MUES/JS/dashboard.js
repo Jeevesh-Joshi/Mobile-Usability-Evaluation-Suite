473321770aebcc180030dc17e2d491b6dbc0b8e7
Jeevesh Chandra Joshi <jeeveshjoshi1999@gmail.com>
2021-11-08 16:18:32 +0530

    Dashboard

--- SSD_project/MUES/static/MUES/JS/dashboard.js
+++ SSD_project/MUES/static/MUES/JS/dashboard.js
@@ -1,5 +1,3 @@
-let users = [
-];
 // Check All
 function check(checked = true) {
   const cbs = document.querySelectorAll('input[name="tasks"]');
@@ -19,71 +17,23 @@ function uncheckAll(e) {
   // reassign click event handler
   this.onclick = checkAll;
 }
+
 const btn = document.querySelector("#checkBtn");
 btn.onclick = checkAll;
 
-// Delete User
-let deleteUser = (index) => {
-  users.splice(index, 1);
-  console.log(`element deleted at index${index}`);
-  printData();
-};
-
-// Print data to the table
-// let printData = () => {
-//   document.getElementById("data").innerHTML = "";
-//   let i = 0;
-//   users.map((x) => {
-//     document.getElementById("data").innerHTML += `
-//         <tr>
-//         <td >${i + 1}</td>
-//         <td >${x.name}</td>
-//         <td >${x.age}</td>
-//         <td >${x.gender}</td>
-//         <td >${x.tasks}</td>
-//         <td > <button class="btn btn-outline-danger btn-sm" onClick="deleteUser(${i})"><i class="bi bi-trash"></i></button> </td>
-//         </tr>
-//         `;
-//     i += 1;
-//   });
-  // console.log("printdata");
-// };
-printData();
-
 // Get input from the form and add to the table
 let addData = (e) => {
-  // e.preventDefault();
-  console.log("Added data");
   let name = document.getElementById("name").value;
   let age = parseInt(document.getElementById("age").value);
-  let radiobts = document.querySelectorAll('input[name="gender"]');
   let checkboxes = document.querySelectorAll('input[name="tasks"]:checked');
   let tasks = [];
-  let gender;
-  for (let rbt of radiobts) {
-    if (rbt.checked) {
-      gender = rbt.value;
-      //   console.log(gender)
-      break;
-    }
-  }
   checkboxes.forEach((checkbox) => {
     tasks.push(checkbox.value);
   });
-  console.log(tasks);
-  let dataToAdd = {
-    name,
-    age,
-    gender,
-    tasks,
-  };
-  if (name.length && age && tasks.length > 0) {
-    users.push(dataToAdd);
-  } else {
-    alert("Something is missing");
+  if(!(name.length && age && tasks.length)){
+    e.preventDefault();
+    alert("Incomplete form!");
   }
-  document.getElementById("userDetails").reset();
-  printData();
 };
 
-// document.getElementById("s").addEventListener("click", addData);
+document.getElementById("s").addEventListener("click", addData);
--- SSD_project/MUES/views.py
+++ SSD_project/MUES/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from MUES.models import Tasks,Users,Videos
 
 # Create your views here.
 
-
 def index(request):
-    # return HttpResponse("Hello inside app")
     return render(request,"MUES/index.html")
 
 def dashboard(request):
     tasks = Tasks.objects.values()
-    users = Users.objects.values()
+    users = Users.objects.all()
     data = {"tasks":tasks, "users":users}
-    print(data)
+
     if request.method == 'GET':
         return render(request,"MUES/dashboard.html",data)
     else:
@@ -21,9 +18,8 @@ def dashboard(request):
         age = request.POST.get('userage','default')
         gender = request.POST.get('gender','default')
         tasks = request.POST.getlist('tasks','default')
-        
-        tasksdb = Tasks.objects.filter(id__in=tasks)
+        # tasksdb = Tasks.objects.filter(id__in=tasks)
         user = Users(name=name,age=age,gender=gender)
         user.save()
-        user.tasks.add(*tasksdb)
+        user.tasks.add(*tasks)
         return render(request,"MUES/dashboard.html",data)
